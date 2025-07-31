5434d8dd6ce61a1241c38eff31d17d9833ed04e4
varun <varunify.777@gmail.com>
2025-07-31 19:52:29 +0530

    Time: 2 ms (90.67%) | Memory: 45.4 MB (40.97%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,31 +1,33 @@
-//brute force
-
 class Solution {
-    public List<Integer> luckyNumbers(int[][] matrix) {
+    public List<Integer> luckyNumbers(int[][] matrix) { 
         List<Integer> result = new ArrayList<>();
-        for(int i = 0 ;i<matrix.length;i++){
-            int ind1=i;
-            int ind2=0;
-            for(int j = 0 ; j <matrix[i].length;j++){
-                if(matrix[i][j] <matrix[ind1][ind2]){
-                    ind1=i;
-                    ind2=j;
+        int minind1,minind2;
+        for(int i = 0 ; i<matrix.length ; i++){
+            minind1= i;
+            minind2= 0;
+            for(int j = 0 ; j<matrix[i].length; j++){
+                if(matrix[i][j]<matrix[minind1][minind2]){
+                    minind1=i;
+                    minind2=j;
                 }
             }
-            int max = matrix[0][ind2];
-            for(int k = 1 ; k<matrix.length ; k++ ){
-                if(matrix[k][ind2]>max){
-                    max = matrix[k][ind2];
+            int max = matrix[0][minind2];
+            for(int k = 1; k< matrix.length ; k++){
+                if(matrix[k][minind2] > max){
+                    max = matrix[k][minind2];
                 }
             }
-            if(max == matrix[ind1][ind2]){
+            if(matrix[minind1][minind2] == max){
                 result.add(max);
             }
         }
         return result;
-        }
     }
+}
 
-// 3 00     7 01      8 02 
-// 9  10    11 11    13  12
-// 15  20   16  21    17 22
+
+
+// [3,7,8] 0
+// [9,11,13] 1
+// [15,16,17] 2
+
