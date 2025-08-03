6717b0c41679c5e0fe6100191d9e88867d96a041
varun <varunify.777@gmail.com>
2025-08-03 16:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (29.22%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int[] arr = new int[26];
-        for(char c : sentence.toCharArray()){
-            arr[c-'a']=1;
-        }
-        for(int i:arr){
-            if(i==0){
+        for(char c='a';c<='z';c++){
+            if(sentence.indexOf(c)==-1){
                 return false;
             }
         }
