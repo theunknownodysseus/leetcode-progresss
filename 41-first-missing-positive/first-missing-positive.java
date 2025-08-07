338409c99328c104d650aa82bb0b610b025b3add
varun <varunify.777@gmail.com>
2025-08-07 23:37:51 +0530

    Time: 14 ms (25.03%) | Memory: 56.8 MB (84.04%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,7 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         Arrays.sort(nums);
-        int max=nums[nums.length-1];
         int min=1;
         for(int i = 0 ;i<nums.length;i++){
             if(min==nums[i]){
