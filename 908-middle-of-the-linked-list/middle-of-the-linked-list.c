b11839a44476fe11320994c7badb983a89c805e0
varun <varunify.777@gmail.com>
2025-08-23 22:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (58.17%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.c
@@ -0,0 +1,10 @@
+struct ListNode* middleNode(struct ListNode* head) {
+        struct ListNode *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
