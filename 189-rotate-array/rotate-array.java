f762a3f3a845a5e5bd53b713b2fdedfebb5a50a4
varun <varunify.777@gmail.com>
2025-08-27 00:56:11 +0530

    Time: 1 ms (45.98%) | Memory: 57.1 MB (90.91%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -4,14 +4,14 @@ class Solution {
         k = k%n;
         int[] res = new int[n];
         int j = 0;
-        for(int i = n-k;i<n;i++){
-            res[j++] = nums[i];
+        for(int i = n-k; i<n; i++){
+            res[j++]=nums[i];
         }
-        for(int i = 0 ; i<n-k;i++){
-            res[j++] = nums[i];
+        for(int i = 0; i<n-k;i++){
+            res[j++]=nums[i];
         }
-        for(int i = 0 ; i <n ;i++){
-            nums[i]=res[i];
+        for(int i = 0; i<n ; i++){
+            nums[i] = res[i];
         }
     }
 }
