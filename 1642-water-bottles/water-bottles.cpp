5f86a6ceee93b8e23c0321c237b61b384465d261
varun <varunify.777@gmail.com>
2025-10-01 17:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int res = numBottles;
+        while(numBottles>=numExchange){
+            int rem = numBottles%numExchange;
+            numBottles/=numExchange;
+            res+=numBottles;
+            numBottles+=rem;
+        }
+        return res;
+    }
+};
