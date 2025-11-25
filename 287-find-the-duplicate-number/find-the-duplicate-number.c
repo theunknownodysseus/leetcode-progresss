65f65540cdc3af5a9829350f770e69bc588a66de
varun <varunify.777@gmail.com>
2025-11-25 19:48:22 +0530

    Time: 2 ms (75.40%) | Memory: 14.3 MB (34.62%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.c
@@ -0,0 +1,14 @@
+int findDuplicate(int* nums, int numsSize) {
+    int fast = nums[0] ;
+    int slow = nums[0] ;
+    do{
+        fast = nums[nums[fast]];
+        slow = nums[slow];
+    }while(fast != slow);
+    fast = nums[0];
+    while(slow!=fast){
+        fast = nums[fast];
+        slow = nums[slow];
+    }
+    return fast;
+}
