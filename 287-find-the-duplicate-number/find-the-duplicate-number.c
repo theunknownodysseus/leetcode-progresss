627ec6ca543b21156e23debd586f5845fe36b0db
varun <varunify.777@gmail.com>
2025-11-25 19:56:33 +0530

    Time: 3 ms (70.65%) | Memory: 14 MB (98.24%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.c
+++ 287-find-the-duplicate-number/find-the-duplicate-number.c
@@ -1,14 +1,28 @@
 int findDuplicate(int* nums, int numsSize) {
-    int fast = nums[0] ;
-    int slow = nums[0] ;
-    do{
-        fast = nums[nums[fast]];
-        slow = nums[slow];
-    }while(fast != slow);
-    fast = nums[0];
-    while(slow!=fast){
-        fast = nums[fast];
-        slow = nums[slow];
+    // int fast = nums[0] ;
+    // int slow = nums[0] ;
+    // do{
+    //     fast = nums[nums[fast]];
+    //     slow = nums[slow];
+    // }while(fast != slow);
+    // fast = nums[0];
+    // while(slow!=fast){
+    //     fast = nums[fast];
+    //     slow = nums[slow];
+    // }
+    // return fast;
+
+    int freq[numsSize];
+    for(int i = 0 ; i<numsSize ; i++){
+        freq[i]=0;
     }
-    return fast;
+    for(int i = 0 ;i<numsSize ; i++){
+        if(freq[nums[i]]==0){
+            freq[nums[i]]++;
+        }
+        else{
+            return nums[i];
+        }
+    }
+    return -1;
 }
