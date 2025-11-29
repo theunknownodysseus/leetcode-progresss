903b97e96c83cc1286e095d893ed35a92da6e02c
varun <varunify.777@gmail.com>
2025-11-29 11:34:29 +0530

    Time: 4 ms (40.00%) | Memory: 84.5 MB (12.00%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.c
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.c
@@ -1,56 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
 struct ListNode* reverse(struct ListNode* start , struct ListNode* end){
-    struct ListNode* prev = end; 
+    
+    struct ListNode* prev = end ; 
     struct ListNode* curr = start;
     struct ListNode* nextnode;
 
-    while (curr != end) {
+    while(curr!=end){
         nextnode = curr->next;
         curr->next = prev;
         prev = curr;
-        curr = nextnode;
+        curr= nextnode;
     }
     return prev;
 }
 
 struct ListNode* reverseEvenLengthGroups(struct ListNode* head) {
-
-    if (!head) return head;
-
+    if(!head){
+        return head;
+    }
     int k = 1;
     struct ListNode dum;
     dum.next = head;
-
     struct ListNode* prevgrptail = &dum;
     struct ListNode* start = head;
-
-    while (start) {
-
+    
+    while(start){
         struct ListNode* end = start;
-
-        
-        int count = 0;
-        for (int i = 0; i < k && end; i++) {
-            end = end->next;
+        int count  = 0 ;
+        for(int i = 0 ; i < k && end ; i++){
+            end=end->next;
             count++;
         }
-
-        
-        if (count % 2 == 0) {
-            struct ListNode* newhead = reverse(start, end);
-            prevgrptail->next = newhead;
-            prevgrptail = start;
-        }
-        
+        if(count%2==0){
+        struct ListNode* newhead = reverse(start,end);
+        prevgrptail->next = newhead;
+        prevgrptail = start;}
         else {
             prevgrptail->next = start;
             for (int i = 0; i < count; i++) {
                 prevgrptail = prevgrptail->next;
             }
         }
-
         start = end;
         k++;
     }
-
     return dum.next;
 }
