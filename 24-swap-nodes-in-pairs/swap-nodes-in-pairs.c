39e4cb55a32555c588f7c122e938fe3479e1c4ea
varun <varunify.777@gmail.com>
2025-11-29 18:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (99.63%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.c
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* start , struct ListNode* end) {
+    
+    struct ListNode* prev = end;
+    struct ListNode* curr = start;
+    struct ListNode* nextnode;
+
+
+    while(curr!=end){
+        nextnode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextnode;
+    }
+    return prev;
+    
+}
+
+struct ListNode* swapPairs(struct ListNode* head) {
+    int k = 2;
+    if(!head){
+        return head;
+    }
+
+    struct ListNode dum;
+    dum.next = head;
+    struct ListNode* prevgrptail = &dum;
+    struct ListNode* start = head;
+    struct ListNode* end = head;
+    struct ListNode* newhead = NULL;
+    while(1){
+        for(int i = 0 ; i < k ; i++){
+            if(!end){
+                return dum.next;
+            }
+            end=end->next;
+        }
+        newhead = reverseList(start,end);
+        prevgrptail->next = newhead;
+        prevgrptail = start;
+        start = end;
+    }
+    return dum.next;
+}
