dec8bd7fd76dd9c22e4067af0987bb8beb23e642
varun <varunify.777@gmail.com>
2025-12-01 21:26:16 +0530

    Time: 7 ms (8.71%) | Memory: 47.7 MB (22.61%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,28 +1,43 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int count = 0;
-        for(int i : nums){
+        // int count = 0;
+        // for(int i : nums){
 
-            if(i==0){ 
-            count++;
+        //     if(i==0){ 
+        //     count++;
+        //     }
+
+        // }
+
+        // int ind = 0;
+        // for(int i = 0 ; i < nums.length ; i++){
+
+        //     if(nums[i]!=0){
+        //         nums[ind] = nums[i];
+        //         ind++;
+        //     }
+
+        // }  0,1,0,3,12
+
+        // count = nums.length - count;
+        // for(int i  = count ; i<nums.length ; i++){
+        //     nums[i] = 0;
+        // }
+
+        int i , j;
+        i = 0;
+        j = 0;
+        while(j<nums.length){
+            if(nums[j]!=0)
+            {   
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
             }
-
+            j++;
         }
-
-        int ind = 0;
-        for(int i = 0 ; i < nums.length ; i++){
-
-            if(nums[i]!=0){
-                nums[ind] = nums[i];
-                ind++;
-            }
-
-        }
-
-        count = nums.length - count;
-        for(int i  = count ; i<nums.length ; i++){
-            nums[i] = 0;
-        }
+        System.out.println(i +" "+ j);
     }
 }
