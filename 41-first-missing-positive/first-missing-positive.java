4d5ec4a1ab2c6fa83f334d0c675bc2596bc1eb34
varun <varunify.777@gmail.com>
2026-09-07 11:41:00 +0530

    Time: 1 ms (99.95%) | Memory: 74.5 MB (20.14%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,14 +1,21 @@
 class Solution {
+
     public int firstMissingPositive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1]; 
+
         for (int num : nums) {
-            if (num > 0) set.add(num);
+            if (num > 0 && num <= n) {
+                seen[num] = true;
+            }
+        }                                                         
+
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                return i;
+            }
         }
 
-        for (int i = 1; i <= nums.length + 1; i++) {
-            if (!set.contains(i)) return i;
-        }
-
-        return -1; 
+        return n + 1;
     }
 }
